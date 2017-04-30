6de664a11789c47c3a1c65b8599c14e911382989
Michał Budzyński <michal@virtualdesign.pl>
2017-04-30 19:47:00 +0100

    :chestnut: bundle with rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,28 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import serve from 'rollup-plugin-serve'
+import livereload from 'rollup-plugin-livereload'
+
 export default {
   entry: 'src/main.js',
   format: 'iife',
   moduleName: 'Fruu',
-  dest: 'dist/fruu.js'
+  dest: 'dist/fruu.js',
+  plugins: [
+    resolve({
+      customResolveOptions: {
+        moduleDirectory: 'node_modules'
+      }
+    }),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    serve({
+      // open: true,
+      contentBase: ['public', 'dist']
+    }),
+    livereload({
+      watch: ['dist', 'public']
+    })
+  ],
 };
